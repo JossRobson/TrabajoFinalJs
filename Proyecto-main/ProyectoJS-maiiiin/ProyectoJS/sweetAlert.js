b6cf9db3e192cefa928f0cbda1c4e18ae7c4ebb9
JossRobson <100178421+JossRobson@users.noreply.github.com>
2023-04-20 00:23:58 -0300

    Update sweetAlert.js

--- Proyecto-main/ProyectoJS-maiiiin/ProyectoJS/sweetAlert.js
+++ Proyecto-main/ProyectoJS-maiiiin/ProyectoJS/sweetAlert.js
@@ -1,2 +1,15 @@
-Swal.fire('Gracias por su compra!' , 'Su pedido esta siendo procesado!' , 'succes');
+//Swal.fire('Gracias por su compra!' , 'Su pedido esta siendo procesado!' , 'succes' , 'comprar');
+
+
+  
+const comprarBtn = document.getElementById('comprar-btn');
+
+comprarBtn.addEventListener('click', function() {
+  Swal.fire({
+    title: 'Compra realizada!',
+    text: 'Su compra está siendo procesada.',
+    icon: 'success',
+    confirmButtonText: 'OK'
+  });
+});
 
